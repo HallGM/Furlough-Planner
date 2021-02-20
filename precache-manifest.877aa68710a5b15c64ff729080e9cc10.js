c2d162debbc6c4e3660566f2841dab6dd1e93ba3
Garry Hall <garrymhall@gmail.com>
2021-02-20 21:13:56 +0000

    Updates

---
+++ precache-manifest.877aa68710a5b15c64ff729080e9cc10.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5ae406a2c39a23dabe2582e6530749",
+    "url": "/Furlough-Planner/index.html"
+  },
+  {
+    "revision": "b7acaca16531494fb089",
+    "url": "/Furlough-Planner/static/css/main.e1bc4955.chunk.css"
+  },
+  {
+    "revision": "e6a5d38134a8b763d4c3",
+    "url": "/Furlough-Planner/static/js/2.25758992.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Furlough-Planner/static/js/2.25758992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7acaca16531494fb089",
+    "url": "/Furlough-Planner/static/js/main.b92ebb31.chunk.js"
+  },
+  {
+    "revision": "e5506b44092d6c06e98e",
+    "url": "/Furlough-Planner/static/js/runtime-main.e30f6f15.js"
+  },
+  {
+    "revision": "7dda32825225f4d58ce3d4edb0537f17",
+    "url": "/Furlough-Planner/static/media/kitchen.7dda3282.jpg"
+  },
+  {
+    "revision": "23a332a4d0be3b6d2cd4c67eaeb2a52d",
+    "url": "/Furlough-Planner/static/media/living-room.23a332a4.jpg"
+  },
+  {
+    "revision": "94a0c038c0e79c401bea341171206c5b",
+    "url": "/Furlough-Planner/static/media/office.94a0c038.jpg"
+  },
+  {
+    "revision": "bba45e7a7217606e55897f9cf638cd36",
+    "url": "/Furlough-Planner/static/media/outside.bba45e7a.jpg"
+  },
+  {
+    "revision": "51b8e6d0411f4f84cc35f37d147d2c1e",
+    "url": "/Furlough-Planner/static/media/paper.51b8e6d0.jpg"
+  }
+]);
